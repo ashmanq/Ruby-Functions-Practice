ae75d3ea415a45673fa01ba5384a4d4e95eb3be2
David Eastcroft <davideastcroft95@gmail.com>
2020-03-10 16:08:38 +0000

    Added return and add function.

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -2,10 +2,10 @@ def return_10
   return 10
 end
 
-# def add(first, second)
-#   return first + second
-# end
-#
+def add(first, second)
+  return first + second
+end
+
 # def subtract(first, second)
 #   return first - second
 # end
