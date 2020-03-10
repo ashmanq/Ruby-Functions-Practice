a7a3e6aad99af2766c92b2b330d5c3ce82376afd
David Eastcroft <davideastcroft95@gmail.com>
2020-03-10 16:07:28 +0000

    Initial commit.

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,53 @@
+def return_10
+  return 10
+end
+
+# def add(first, second)
+#   return first + second
+# end
+#
+# def subtract(first, second)
+#   return first - second
+# end
+#
+# def multiply(first, second)
+#   return first * second
+# end
+#
+# def divide(first, second)
+#   return first / second
+# end
+#
+# def length_of_string(test_string)
+#   return test_string.length
+# end
+#
+# def join_string(string_1, string_2)
+#   return string_1 + string_2
+# end
+#
+# def add_string_as_number(string_1, string_2)
+#   return string_1.to_i + string_2.to_i
+# end
+#
+# def number_to_full_month_name(month_num)
+#   case month_num
+#   when 1
+#     p "January"
+#   when 3
+#     p "March"
+#   when 9
+#     p "September"
+#   end
+# end
+#
+# def number_to_short_month_name(month_num)
+#   case month_num
+#   when 1
+#     p "Jan"
+#   when 4
+#     p "Apr"
+#   when 10
+#     p "Oct"
+#   end
+# end
